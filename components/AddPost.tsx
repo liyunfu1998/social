6d5ad5360ee37e116e945ec7525042706e324d83
liyunfu1998 <liyunfu1998@qq.com>
2024-08-17 20:58:48 +0800

    feat: add addpost component

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,5 +1,53 @@
-export default function AddPost(){
+import Image from "next/image";
+
+export default function AddPost() {
   return (
-    <div className="">AddPost</div>
-  )
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/27819652/pexels-photo-27819652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 rounded-full"
+      />
+      {/* POST */}
+      <div className="flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea
+            placeholder="What's on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+          ></textarea>
+          <Image
+            src="/images/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/images/addimage.png" alt="" width={20} height={20} />
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/images/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/images/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/images/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
